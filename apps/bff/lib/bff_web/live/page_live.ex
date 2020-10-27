41feb88a1162ed8c7646f6203d11de4ce8da95ca
kokolegorille <koko.le.gorille@gmail.com>
2020-10-27 22:51:06 +0100

    Add bootstrap to bff

--- apps/bff/lib/bff_web/live/page_live.ex
+++ apps/bff/lib/bff_web/live/page_live.ex
@@ -5,4 +5,30 @@ defmodule BffWeb.PageLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def handle_params(_params, _url, socket) do
+    events = EventStore.list_events(order: :asc)
+    {:noreply, assign(socket, events: events)}
+  end
+
+  defp display_json(json) do
+    Enum.map(json, fn {key, value} ->
+      "#{key}=#{truncate value}\r\n"
+    end)
+  end
+
+  defp truncate(input, length \\ 30)
+
+  defp truncate(nil, _length) do
+    "nil"
+  end
+
+  defp truncate(input, length) do
+    if String.length(input) > length do
+      String.slice(input, 0..length) <> "..."
+    else
+      input
+    end
+  end
 end
--- apps/emailer/lib/emailer/core/event_handlers.ex
+++ apps/emailer/lib/emailer/core/event_handlers.ex
@@ -18,8 +18,4 @@ defmodule Emailer.Core.EventHandlers do
   def handle(command) do
     Logger.info("#{__MODULE__} Unknown Command #{inspect(command)}")
   end
-
-  # defp create_event(event) do
-  #   EventStore.create_event(event)
-  # end
 end
